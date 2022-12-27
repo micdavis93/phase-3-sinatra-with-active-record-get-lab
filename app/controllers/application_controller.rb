f2e4f01eed3f60437355992ecf532738ec9a11e4
Michael Davis <micdavis93@gmail.com>
2022-12-27 10:56:45 -0700

    commit tree 1cfec282f6c89e5d3e04d2a65f900c99f59a2ce7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,33 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  # add routes
+  get '/bakeries' do
+    # get all the bakeries from the database
+    bakeries = Bakery.all
 
+    # return a JSON response with an array of all the bakery data
+    bakeries.to_json
+  end
+
+  # get 'bakeries/:id' do
+  #   bakery = Bakery.find(params[:id])
+
+  #   bakery.to_json(include: :baked_goods)
+  # end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  # get '/games/:id' do
+  #   # look up the game in the database using its ID
+  #   game = Game.find(params[:id])
+  #   # send a JSON-formatted response of the game data
+  #   game.to_json(only: [:id, :title, :genre, :price, :platform], include: {
+  #     reviews: { only: [:comment, :score], include: {
+  #       user: { only: [:name] }
+  #     } }
+  #   })
+  # end
 end
